236bcd6c7dd7ca803a254776e71aa808052229f8
Zdenek Prusa <zdenek.prusa@gmail.com>
2015-02-05 14:37:22 +0100

    Initial structure for plugin and standalone

---
+++ plugin/Source/PluginProcessor.cpp
@@ -0,0 +1,192 @@
+/*
+  ==============================================================================
+
+    PluginProcessor.cpp
+    Created: 5 Feb 2015 10:55:12am
+    Author:  susnak
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+PluginAudioProcessor::PluginAudioProcessor()
+{
+}
+
+PluginAudioProcessor::~PluginAudioProcessor()
+{
+}
+
+//==============================================================================
+const String PluginAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+int PluginAudioProcessor::getNumParameters()
+{
+    return 0;
+}
+
+float PluginAudioProcessor::getParameter (int index)
+{
+    return 0.0f;
+}
+
+void PluginAudioProcessor::setParameter (int index, float newValue)
+{
+}
+
+const String PluginAudioProcessor::getParameterName (int index)
+{
+    return String();
+}
+
+const String PluginAudioProcessor::getParameterText (int index)
+{
+    return String();
+}
+
+const String PluginAudioProcessor::getInputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+const String PluginAudioProcessor::getOutputChannelName (int channelIndex) const
+{
+    return String (channelIndex + 1);
+}
+
+bool PluginAudioProcessor::isInputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool PluginAudioProcessor::isOutputChannelStereoPair (int index) const
+{
+    return true;
+}
+
+bool PluginAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PluginAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool PluginAudioProcessor::silenceInProducesSilenceOut() const
+{
+    return false;
+}
+
+double PluginAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int PluginAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int PluginAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void PluginAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String PluginAudioProcessor::getProgramName (int index)
+{
+    return String();
+}
+
+void PluginAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void PluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void PluginAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+void PluginAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // I've added this to avoid people getting screaming feedback
+    // when they first compile the plugin, but obviously you don't need to
+    // this code if your algorithm already fills all the output channels.
+    for (int i = getNumInputChannels(); i < getNumOutputChannels(); ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    int L = buffer.getNumSamples();
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    for (int channel = 0; channel < getNumInputChannels(); ++channel)
+    {
+        float* dst = buffer.getWritePointer (channel);
+        float* src = buffer.getWritePointer (channel);
+        memcpy(dst,src,L*sizeof*dst);
+        // ..do something to the data...
+    }
+}
+
+//==============================================================================
+bool PluginAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* PluginAudioProcessor::createEditor()
+{
+    return new PluginEditor (*this);
+}
+
+//==============================================================================
+void PluginAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void PluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new PluginAudioProcessor();
+}
